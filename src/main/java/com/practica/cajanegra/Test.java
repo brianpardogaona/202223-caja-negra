459f2281b3ad569f524df0838b55c4f4b9e5637e
brianpardogaona <brian.pardo.gaona@alumnos.upm.es>
2023-06-12 18:57:36 +0200

    Añadiendo dependencias

---
+++ src/main/java/com/practica/cajanegra/Test.java
@@ -0,0 +1,10 @@
+package com.practica.cajanegra;
+
+public class Test {
+
+
+    public void Test1(){
+
+    }
+
+}
---
+++ src/test/java/com/practica/cajanegra/TestTest.java
@@ -0,0 +1,12 @@
+package com.practica.cajanegra;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    @Test
+    void test1() {
+    }
+}
